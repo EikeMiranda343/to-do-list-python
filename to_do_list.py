f716f4b919ea1ceb70972bf49d61953f6969f7f6
Eike <eike.miranda@acad.ifma.edu.br>
2025-04-19 05:57:29 -0300

    update

--- to_do_list.py
+++ to_do_list.py
@@ -1,26 +1,47 @@
 tarefas = []
-tarefas_concluidas = {}
 
-def menu ():
+def menu():
+    print('Menu - Escolha uma opção: ')
     print('1. Adicionar tarefa')
-    print('2. Concluir tarefa')
-    print('3. Listar tarefas')
-    print('4. Remover tarefa')
-    print('5. Sair da aplicação')
+    print('2. Listar tarefas')
+    print('3. Remover tarefa')
+    print('4. Marcar tarefa como concluída')
+    print('5. Sair')
+
+def adicionar_tarefa():
+    tarefa = input('Digite a tarefa: ')
+    tarefas.append(tarefa)
+    print(f'Tarefa "{tarefa}" adicionada com sucesso!')
     print()
 
-def adicionar_tarefa ():
-    tarefa = input('Qual tarefa você deseja adicionar? ')
-    nova_tarefa = {"tarefa": tarefa, "concluida": False}
-    tarefas.append(nova_tarefa)
-    print('Tarefa adicionada com sucesso!')
+def listar_tarefa():
+    for i in range(0, len(tarefas)):
+        print(f'{i + 1} - {tarefas[i]}')
+    print()
 
-def listar_tarefa ():
-    if not tarefas:
-        print('Você não adicionou nenhuma tarefa!')
-    else:
-        for i, tarefa in enumerate(tarefa, start = 1):
-            print(tarefa[i])
+def remover_tarefa():
+    listar_tarefa()
+    print()
+    indice = input('Digite o número da tarefa que deseja remover: ')
+    tarefa_removida = tarefas.pop(int(indice) - 1)
+    print(f'Tarefa "{tarefa_removida}" removida com sucesso!')
+    print()
 
+def to_do_list():
+    while True:
+        menu()
+        opcao = int(input('Escolha uma opção: '))
+        print()
+        if opcao == 1:
+            adicionar_tarefa()
+        elif opcao == 2:
+            listar_tarefa()
+        elif opcao == 3:
+            remover_tarefa()
+        elif opcao == 4:
+            listar_tarefa()
+        else:
+            print('Saindo...')
+            break
 
-adicionar_tarefa()
+to_do_list()
